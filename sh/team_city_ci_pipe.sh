1e31e3949c90e4e5f328a61bb8fd0c3a0544ef50
Jon Jagger <jon@jaggersoft.com>
2018-08-15 13:09:58 +0100

    deleted dead code

--- sh/team_city_ci_pipe.sh
+++ sh/team_city_ci_pipe.sh
@@ -5,7 +5,6 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/.env
 
 ${MY_DIR}/docker_build.sh
-#${MY_DIR}/docker_tag.sh
 ${MY_DIR}/container_down.sh
 ${MY_DIR}/container_up.sh
 ${MY_DIR}/container_logs.sh
