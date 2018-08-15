338facb99b4f153f86ff7d55fd2ee75c16e314e0
Jon Jagger <jon@jaggersoft.com>
2018-08-15 07:02:45 +0100

    have to do maven steps in TeamCity build step

--- sh/team_city_ci_pipe.sh
+++ sh/team_city_ci_pipe.sh
@@ -4,10 +4,6 @@ set -ex
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/.env
 
-echo "Build the jar"
-mvn clean
-mvn install
-
 echo "Build an image from the jar"
 docker build \
   --tag ${APP_IMAGE} \
