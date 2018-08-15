94b30e7ea8dc6ed8562cb651b3a58e24b504cde9
Jon Jagger <jon@jaggersoft.com>
2018-08-15 14:33:56 +0100

    refactor curl tests

--- sh/container_ci_smoke_tests.sh
+++ sh/container_ci_smoke_tests.sh
@@ -1,32 +1,33 @@
 #!/bin/bash
-set -ex
+set -x
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/env-vars.sh
 
-echo "Run basic smoke-test"
-readonly CURL_LOG="/tmp/curl-${APP_PORT}.log"
+# - - - - - - - - - - - - - -
 
+curl_route()
+{
+  IP='localhost'
+  ROUTE=$1
+  CURL_LOG="/tmp/curl-${APP_PORT}.log"
+
+  echo "cURLing... http://${IP}:${APP_PORT}${ROUTE}"
+  curl -i -f -X GET "http://${IP}:${APP_PORT}${ROUTE}" &> ${CURL_LOG}
+  status=$?
+  if [ "${status}" -eq "0" ]; then
+    echo "PASS ${status} ${ROUTE}"
+  else
+    echo "FAIL ${status} ${ROUTE}"
+    cat ${CURL_LOG}
+  fi
+}
+
+# - - - - - - - - - - - - - -
+
+echo "Run basic smoke-test"
 # Crude wait for readyness
 sleep 5
 
-if curl -i -X GET "http://localhost:${APP_PORT}/" &> ${CURL_LOG} ; then
-  echo "Route / is 200"
-else
-  status=$?
-  echo "Route / is poorly (${status})"
-  cat ${CURL_LOG}
-  ${MY_DIR}/container_down.sh
-  exit ${status}
-fi
-
-if curl -i -X GET "http://localhost:${APP_PORT}/page1.html" &> ${CURL_LOG} ; then
-  echo "Route /page1.html is 200"
-else
-  status=$?
-  echo "Route /page1.html is poorly (${status})"
-  cat ${CURL_LOG}
-  ${MY_DIR}/container_down.sh
-  exit ${status}
-fi
-
+curl_route '/'
+curl_route '/page1.html'
