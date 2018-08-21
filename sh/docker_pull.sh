3379cf738d423eb92014416e53cd1ff761f24eca
Jon Jagger <jon@jaggersoft.com>
2018-08-21 16:44:44 +0100

    try switching back to using docker registry

--- sh/docker_pull.sh
+++ sh/docker_pull.sh
@@ -6,7 +6,7 @@ source ${MY_DIR}/env-vars.sh
 
 echo "Pull the image from the repository"
 # OFF because it keeps falling over
-# docker pull ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
+docker pull ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
 
 # You can also docker save/load instead of using the registry.
-docker load < ${MY_DIR}/${APP_IMAGE}.tar.gz
+#docker load < ${MY_DIR}/${APP_IMAGE}.tar.gz
--- sh/env-vars.sh
+++ sh/env-vars.sh
@@ -3,4 +3,5 @@ export APP_IMAGE=demo-web-mvc
 export APP_CONTAINER=demo-web-mvc
 export JAR_FILENAME=demowebmvc-0.0.1-SNAPSHOT.jar
 
-export DOCKER_REGISTRY_URL=dockerreg.training.local:5000
+#export DOCKER_REGISTRY_URL=dockerreg.training.local:5000
+export DOCKER_REGISTRY_URL=docker.conygre.com:5000
