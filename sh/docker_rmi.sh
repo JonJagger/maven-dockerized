e7a2f230678680b4ee10a9c57007f2223f29f5f6
Jon Jagger <jon@jaggersoft.com>
2018-08-21 16:16:57 +0100

    don't fail the rm command when they run first time

--- sh/docker_rmi.sh
+++ sh/docker_rmi.sh
@@ -5,12 +5,13 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/env-vars.sh
 
 echo "Remove the local image"
-docker rmi ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
+if ! docker rmi ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
+then
+  echo "no previous image to remove"
+fi
 
 echo "Remove cached images"
-
-#docker rmi $(docker images | tail -n +2 | awk '{print $3}')
-
-#This fails with
-#Error response from daemon: conflict: unable to delete 628fa1bffa5f (must be forced) - image is referenced in multiple repositories
-
+if ! docker rmi $(docker images | tail -n +2 | awk '{print $3}')
+then
+  echo "no cached images to remove"
+fi
