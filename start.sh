5d115280a41214f3e90d4f24d9ae8349bce91288
Jon Jagger <jon@jaggersoft.com>
2018-08-21 10:48:44 +0100

    run netprobe/logstash in start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,43 @@ set -o errexit
 
 readonly JAR_FILENAME=$1
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [[ -n $ELK ]]
+then
+  echo "Setting up elk.training.local in /etc/hosts"
+	if ! grep elk.training.local /etc/hosts
+	then
+		echo "$ELK	elk.training.local" >>/etc/hosts
+	else
+		sed -i "s/^.*elk\.training\.local.*/$ELK	elk.training.local/" /etc/hosts
+	fi
+  cat /etc/hosts
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [[ -d /app/netprobe ]]
+then
+  echo "Starting netprobe"
+	export LOG_FILENAME=/app/log/netprobe.log
+	/app/netprobe/netprobe.linux_64 -port 7036 &
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if [[ -d /app/logstash-6.2.4 ]] && grep 'elk\.training\.local' /etc/hosts >/dev/null 2>&1
+then
+  echo "Starting logstash"
+	/app/logstash-6.2.4/bin/logstash -f /app/logstash-6.2.4/conf -l /app/log &
+fi
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 echo "Starting the app"
-java -jar ${JAR_FILENAME}
+java -jar ${JAR_FILENAME} > /app/log/app.log 2>&1
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 echo "Keep container alive"
 while :
