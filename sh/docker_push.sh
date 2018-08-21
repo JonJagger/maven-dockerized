c4ecfb3dfd14709d4fe6ce72820e352b743708b9
Jon Jagger <jon@jaggersoft.com>
2018-08-21 16:50:01 +0100

    docker.conygre.com gives error 'http: server gave HTTP response to HTTPS client'

--- sh/docker_push.sh
+++ sh/docker_push.sh
@@ -6,9 +6,9 @@ source ${MY_DIR}/env-vars.sh
 
 echo "Push the image to the repository"
 # OFF because it keeps falling over
-docker push \
-  ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
+#docker push \
+#  ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
 
 # You can also docker save/load instead of using the registry.
-#docker save ${DOCKER_REGISTRY_URL}/${APP_IMAGE} \
-#  | gzip > ${MY_DIR}/${APP_IMAGE}.tar.gz
+docker save ${DOCKER_REGISTRY_URL}/${APP_IMAGE} \
+  | gzip > ${MY_DIR}/${APP_IMAGE}.tar.gz
