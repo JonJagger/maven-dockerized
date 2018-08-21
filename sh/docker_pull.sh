91d50f4563dd1cd56710bdae64498ff71754805d
Jon Jagger <jon@jaggersoft.com>
2018-08-21 10:34:17 +0100

    switch to docker save/load as the registry is sickly again

--- sh/docker_pull.sh
+++ sh/docker_pull.sh
@@ -5,4 +5,8 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/env-vars.sh
 
 echo "Pull the image from the repository"
-docker pull ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
+# OFF because it keeps falling over
+# docker pull ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
+
+# You can also docker save/load instead of using the registry.
+docker load < ${MY_DIR}/${APP_IMAGE}.tar.gz
--- sh/docker_push.sh
+++ sh/docker_push.sh
@@ -5,5 +5,9 @@ readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/env-vars.sh
 
 echo "Push the image to the repository"
-docker push \
-  ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
+# OFF because it keeps falling over
+# docker push \
+#  ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
+
+# You can also docker save/load instead of using the registry.
+docker save ${APP_IMAGE} | gzip > ${MY_DIR}/${APP_IMAGE}.tar.gz
