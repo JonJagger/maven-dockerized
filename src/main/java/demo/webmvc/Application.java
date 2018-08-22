66d4ec341b4e61dd41fe7a1bad3eceab237b54bd
Jon Jagger <jon@jaggersoft.com>
2018-08-22 05:12:37 +0100

    reset to no logging ready for using log4j

--- src/main/java/demo/webmvc/Application.java
+++ src/main/java/demo/webmvc/Application.java
@@ -3,16 +3,10 @@ package demo.webmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 @SpringBootApplication
 public class Application {
 
-  private static final Logger logger = LoggerFactory.getLogger(Application.class);
-
 	public static void main(String[] args) {
-    logger.debug("--Hello-from-Chennai--");
 		SpringApplication.run(Application.class, args);
 	}
 }
