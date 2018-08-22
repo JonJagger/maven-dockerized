534dd51033307e10db63719ceb5e409d706360a3
Jon Jagger <jon@jaggersoft.com>
2018-08-22 02:38:04 +0100

    added logback to pom.xml

--- src/main/java/demo/webmvc/Application.java
+++ src/main/java/demo/webmvc/Application.java
@@ -1,22 +1,17 @@
 package demo.webmvc;
 
-import java.nio.file.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-    String message = "Hello,world from Chennai";
-    Path logPath = Paths.get("/app/log/app.log");
-    try {
-        Files.write(logPath, message.getBytes());
-    }
-    catch (java.io.IOException error) {
-        System.out.println(error.getMessage());
-    }
-
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 		SpringApplication.run(Application.class, args);
+    logger.debug("--Application Started--");
 	}
 }
