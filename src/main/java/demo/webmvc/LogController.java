e5026c54285928caa6779b8c3b26adad0768985e
essjcee <stuartjohncole@gmail.com>
2018-08-22 06:44:11 +0100

    Added for logging

---
+++ src/main/java/demo/webmvc/LogController.java
@@ -0,0 +1,16 @@
+package demo.webmvc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LogController {
+	Logger logger = LoggerFactory.getLogger(LogController.class);
+	@RequestMapping("/logger")
+	public String loghandler() {
+		logger.warn("Log link clicked");
+		return "log";
+	}
+}
