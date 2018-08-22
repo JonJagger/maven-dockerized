360f6327a1f6d8e669775e3caf126b3287067ca9
Jon Jagger <jon@jaggersoft.com>
2018-08-22 02:16:15 +0100

    fix incorrect log path

--- sh/env-vars.sh
+++ sh/env-vars.sh
@@ -23,8 +23,7 @@ export USE_REGISTRY=false
 # The following env-vars do not need to be edited.
 # - - - - - - - - - - - - - - - - - - - - - - - - -
 # The port the app will run on.
-#export APP_PORT=80
-export APP_PORT=9045
+export APP_PORT=80
 
 # The name of the docker image which contains the jar file.
 export APP_IMAGE=${PROJECT_NAME}
--- src/main/java/demo/webmvc/Application.java
+++ src/main/java/demo/webmvc/Application.java
@@ -9,7 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
     String message = "Hello,world from Chennai";
-    Path logPath = Paths.get("/tmp/app/log/app.log");
+    Path logPath = Paths.get("/app/log/app.log");
     try {
         Files.write(logPath, message.getBytes());
     }
