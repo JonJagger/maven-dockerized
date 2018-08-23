2730e11e123d0a19b0df96ae5b7ed801ab78e076
Jon Jagger <jon@jaggersoft.com>
2018-08-23 05:02:12 +0100

    turn off bind-mount

--- sh/container_up.sh
+++ sh/container_up.sh
@@ -5,17 +5,11 @@ set -o xtrace
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 source ${MY_DIR}/env-vars.sh
 
-# Create bind-mount of the log-dir (inside the container)
-# into /tmp on the host so logs are visible on the host.
-readonly APP_LOG_DIR=/app/log
-mkdir -p /tmp/${APP_LOG_DIR}
-
 echo "Bring up the new container"
 docker run \
   --detach \
   --name ${APP_CONTAINER} \
   --hostname chennai-${PROJECT_NAME} \
-  --mount type=bind,source=/tmp/${APP_LOG_DIR},target=${APP_LOG_DIR} \
   --publish ${APP_PORT}:80 \
   --publish 8080:8080 \
     ${DOCKER_REGISTRY_URL}/${APP_IMAGE}
